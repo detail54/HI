ffdab06c9ea10cf6fca87f72de3541cc9f74edc7
전대현 <detail54@nate.com>
2021-02-23 18:21:52 +0900

    21.02.23

---
+++ src/codingtest/codingtest3.java
@@ -0,0 +1,50 @@
+package codingtest;
+
+import java.util.Stack;
+
+class Solution {
+	public int solution(int[][] board, int[] moves) {
+
+		Stack<Integer> stack = new Stack<>();
+
+		int answer = 0;
+
+		for (int i = 0; i < moves.length; i++) {
+			int j = 0;
+			while (true) {
+				if (board[j][moves[i] - 1] == 0) {
+					j++;
+					continue;
+				} else {
+					if (stack.empty() == true) {
+						stack.push(board[j][moves[i] - 1]);
+						board[j][moves[i] - 1] = 0;
+					} else if (stack.peek() == board[j][moves[i] - 1]) {
+						stack.pop();
+						answer += 2;
+					} else {
+						stack.push(board[j][moves[i] - 1]);
+						board[j][moves[i] - 1] = 0;
+					}
+					break;
+				}
+			}
+		}
+		return answer;
+	}
+}
+
+public class codingtest3 {
+
+	public static void main(String[] args) {
+
+		int in1[][] = { { 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 3 }, { 0, 2, 5, 0, 1 }, { 4, 2, 4, 4, 2 }, { 3, 5, 1, 3, 1 } };
+
+		int in[] = { 1, 5, 3, 5, 1, 2, 1, 4 };
+
+		Solution ss = new Solution();
+		ss.solution(in1, in);
+
+	}
+
+}
